054abc71530efb20a5f70694fcce79d84980a145
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-10-29 10:33:03 +0300

    7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+ import React from 'react'
+import {ColorPicker} from "./ColorPicker/ColorPicker"
+const colorPickerOptions = [
+  { label: "red", color: "red"},
+  { label: "green", color: "green"},
+  { label: "blue", color: "blue"},
+  { label: "grey", color: "grey"},
+  { label: "pink", color: "pink"},
+  { label: "indigo", color: "indigo"},
+  { label: "orange", color: "orange"},
+  { label: "lime", color: "lime"},
+]
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className='colorPicker'>
+     <h1>Состояние компонента</h1>
+     <ColorPicker options={colorPickerOptions}/>
     </div>
-  );
+  )
 };
